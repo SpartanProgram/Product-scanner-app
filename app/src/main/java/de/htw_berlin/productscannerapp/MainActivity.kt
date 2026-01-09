40b0cf877d18faef28c9c86196d1d910dd014d11
Zul Fahmi Nur Vagala <zulfahmi0878@gmail.com>
2026-01-09 23:50:37 +0100

    Fix duplicate MainActivity

--- app/src/main/java/de/htw_berlin/productscannerapp/MainActivity.kt
+++ app/src/main/java/de/htw_berlin/productscannerapp/MainActivity.kt
@@ -3,45 +3,16 @@ package de.htw_berlin.productscannerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import de.htw_berlin.productscannerapp.ui.app.ProductScannerApp
 import de.htw_berlin.productscannerapp.ui.theme.ProductScannerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ProductScannerAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ProductScannerApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProductScannerAppTheme {
-        Greeting("Android")
     }
 }
